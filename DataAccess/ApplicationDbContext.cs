d205b77a497f83e0b2050c5dd818292f76358b69
Sebastian Green <sebastianpetergreen@outlook.com>
2020-08-26 19:18:37 +0200

    Initial commit

---
+++ DataAccess/ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using DataAccess.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataAccess
+{
+    public class ApplicationDbContext : IdentityDbContext<User>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+    }
+}
